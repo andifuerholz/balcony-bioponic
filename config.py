b51dc2ba75878de8bddf7104e1af7cf8fb990ccd
andifuerholz <andi.fuerholz@gmail.com>
2026-03-13 18:47:14 +0100

    Add sensor map and temperature limits in config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+SENSOR_MAP = {
+    b'(\x8e\x01H\xf6u<\xde': 'air',   # dein erster Sensor = Luft
+    # b'\x28\x..' : 'water',         # weitere Sensoren später hier ergänzen
+}
+TEMP_MIN = -20.0
+TEMP_MAX =  60.0
